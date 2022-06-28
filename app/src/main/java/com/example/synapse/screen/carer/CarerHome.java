11712ab80a5d0eb7e88a5766f1ec7e4ae060c5b4
PizaaRiaaa <56040946+PizaaRiaaa@users.noreply.github.com>
2022-06-28 22:25:51 +0800

    Complete UI for carer homescreen

--- app/src/main/java/com/example/synapse/screen/carer/CarerHome.java
+++ app/src/main/java/com/example/synapse/screen/carer/CarerHome.java
@@ -1,14 +1,27 @@
 package com.example.synapse.screen.carer;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
 import com.example.synapse.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import android.annotation.SuppressLint;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.style.ForegroundColorSpan;
 
 public class CarerHome extends AppCompatActivity {
 
+    @SuppressLint("RestrictedApi")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_carer_home);
+
+        // set bottomNavigationView to transparent
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setBackgroundColor(ContextCompat.getColor(this, android.R.color.transparent));
+
     }
 }
